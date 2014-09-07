2c51cf81c57d20071568a052f8bb1ac3f3550600
Miklos Danka <mdanka@palantir.com>
2014-09-07 23:41:39 +0200

    Added barkochbatortenet models

--- barkochba/models.py
+++ barkochba/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Story(models.Model):
+	# A tortenet rovid cime
+	title = models.CharField(
+		u'C\u00EDm',
+		max_length=200)
+	# Maga a tortenet amit elmondunk
+	story = models.TextField(
+		u'T\u00F6rt\u00E9net')
+	# A teljes megoldas
+	solution = models.TextField(
+		u'Megold\u00E1s')
+	# Azok, akik mar hallottak korabban
+	people = models.ManyToManyField(Person)
+
+
+
+class Person(models.Model):
+	name = models.CharField(
+		u'N\u00E9v',
+		max_length=200)
