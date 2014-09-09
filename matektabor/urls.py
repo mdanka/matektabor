c2c92269044f41035c70d6602ac0f11a622b6eb8
Miklos Danka <mdanka@palantir.com>
2014-09-09 20:35:59 +0200

    Namespaced the URL

--- matektabor/urls.py
+++ matektabor/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
 	# Examples:
 	# url(r'^$', 'matektabor.views.home', name='home'),
 	# url(r'^blog/', include('blog.urls')),
-	
-	url(r'^barkochba/', include('barkochba.urls')),
+
+	url(r'^barkochba/', include('barkochba.urls', namespace="barkochba")),
 	url(r'^admin/', include(admin.site.urls)),
 )
