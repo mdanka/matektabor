ba6c60fa19c60201f644a7554c89528d462ce2db
Miklos Danka <mdanka@palantir.com>
2014-09-09 20:07:06 +0200

    A barkochba admin lap most mar a sorszamot is mutatja

--- barkochba/admin.py
+++ barkochba/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from barkochba.models import Person, Story
 
-admin.site.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+	list_display = ('title', 'order_number')
+
+admin.site.register(Story, StoryAdmin)
 admin.site.register(Person)
