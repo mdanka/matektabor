2aa31b14ea1e1ed09d9d9fa2e978a3e5f25d7fd3
Miklos Danka <danka.miklos@gmail.com>
2014-12-26 18:52:00 +0100

    The barkochba admin site doesn't allow editing people anymore

--- barkochba/admin.py
+++ barkochba/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from barkochba.models import Story
 
 class StoryAdmin(admin.ModelAdmin):
-	list_display = ('title', 'order_number')
+    list_display = ('title', 'order_number')
+    exclude = ('people',)
 
 admin.site.register(Story, StoryAdmin)
 
