da66ac145e675e9f62f3c5d208a88afb42a74ebf
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2015-11-08 18:45:07 -0500

    Recreated migrations, there might have been a django version issue.

--- szobabeosztas/migrations/0001_initial.py
+++ szobabeosztas/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tabor', '0004_campgroup'),
+        ('tabor', '0004_auto_20151109_0042'),
     ]
 
     operations = [
@@ -16,9 +16,11 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('number', models.CharField(max_length=200, verbose_name='Sorsz\xe1m')),
-                ('group', models.ForeignKey(to='tabor.CampGroup')),
+                ('group', models.ForeignKey(verbose_name='Csoport', to='tabor.CampGroup')),
             ],
             options={
+                'verbose_name': 'T\xe1bor',
+                'verbose_name_plural': 'T\xe1borok',
             },
             bases=(models.Model,),
         ),
@@ -27,10 +29,12 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=200, verbose_name='Szoban\xe9v')),
-                ('camp', models.ForeignKey(to='szobabeosztas.Camp')),
-                ('people', models.ManyToManyField(to='tabor.Person', blank=True)),
+                ('camp', models.ForeignKey(verbose_name='T\xe1bor', to='szobabeosztas.Camp')),
+                ('people', models.ManyToManyField(to='tabor.Person', verbose_name='Lak\xf3k', blank=True)),
             ],
             options={
+                'verbose_name': 'Szoba',
+                'verbose_name_plural': 'Szob\xe1k',
             },
             bases=(models.Model,),
         ),
--- tabor/migrations/0004_auto_20151109_0042.py
+++ tabor/migrations/0004_auto_20151109_0042.py
@@ -19,8 +19,12 @@ class Migration(migrations.Migration):
             ],
             options={
                 'verbose_name': 'T\xe1bori csoport',
-                'verbose_name_plural': 'T\xe1bori csoport',
+                'verbose_name_plural': 'T\xe1bori csoportok',
             },
             bases=(models.Model,),
+        ),
+        migrations.AlterModelOptions(
+            name='person',
+            options={'verbose_name': 'Szem\xe9ly', 'verbose_name_plural': 'Szem\xe9lyek'},
         ),
     ]
